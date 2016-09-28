f5c6e802ebc26727c1f2c8222fa91efc5862eded
jadefeng <jade.jiefeng@gmail.com>
2016-09-28 15:32:39 -0700

    added details to the wine

---
+++ Wine Choices/WhichWineToDrink/index.js
@@ -0,0 +1,388 @@
+/**
+ * This is a wine recommendation decision tree. 
+ * It will help the user pick the wine of their preference based on the occasion, their meal type, and the weather.
+ * It will also teach the user a few details about the different wines and varieties of grapes.
+ */
+
+var Alexa = require('alexa-sdk');
+
+var states = {
+    STARTMODE: '_STARTMODE',                // Prompt the user to start or restart the recommender.
+    ASKMODE: '_ASKMODE',                    // Alexa is asking user the questions.
+    DESCRIPTIONMODE: '_DESCRIPTIONMODE'     // Alexa is describing the final choice and prompting to start again or quit
+};
+
+// Occasion—–> Flavor—–>Price
+
+// Questions
+        // For the sake of testing, moving the immediate YES to #8
+        var nodes = [{ "node": 1, "message": "Are you pairing this wine with a meal?", "yes": 8, "no": 5 },
+// var nodes = [{ "node": 1, "message": "Are you pairing this wine with a meal?", "yes": 2, "no": 5 },
+             { "node": 2, "message": "Does this meal include meat?", "yes": 4, "no": 3 },
+             { "node": 3, "message": "Are you eating a dessert?", "yes": 11, "no": 10 },
+             { "node": 4, "message": "Are you eating red meat like steak or lamb?", "yes": 12, "no": 9 },
+             { "node": 5, "message": "Are you celebrating?", "yes": 7, "no": 6 },
+             { "node": 6, "message": "Is it hot today?", "yes": 13, "no": 14 },
+             { "node": 7, "message": "Do you like sparkling wines?", "yes": 8, "no": 15 },
+
+// Answers & descriptions
+             { "node": 8, "message": "Champagne", "yes": 0, "no": 0, "description": "Champagne can be made from nearly any grape type, and they contain carbon dioxide bubbles to create their fizziness. Technically, champagne is only made in the Champagne region in France. Comparable wines are called sparkling wines." },
+             { "node": 9, "message": "Pinot noir", "yes": 0, "no": 0, "description": "Pinot Noir is the primary red grape in France's Burgundy region. It produces smooth wines with ripe silky tannins and juicy red summer fruit. It is suitable for almost all ocassions and pairs well, like many white wine, with white meat or heavier taste profile vegetable fishes."},
+             { "node": 10, "message": "Sauvignon blanc", "yes": 0, "no": 0 , "description": "Sauvignon blanc is a versatile food-pairing wine for seafood, poultry, and vegetables. The dominating flavors range from sour green fruits of apples, pears and gooseberries through to tropical fruits of melon, mango and blackcurrant."},
+             { "node": 11, "message": "Port or sweet dessert wine", "yes": 0, "no": 0 , "description": "Port wine is made in the Northern part of Portugal along the Douro river. There are many types of port wines that vary in sweetness. Comparable wines include sherry and madeira."},
+             { "node": 12, "message": "Cabernet sauvignon", "yes": 0, "no": 0 , "description": "Cabernet savignon is best with simply prepared red meat. It is full-bodied, but firm and gripping when young, but lightens with age."},
+             { "node": 13, "message": "Rose", "yes": 0, "no": 0 , "description": "Rose wine is very versatile, but wonderful when chilled on hot days. Rose is a dry wine that is brighter and more refreshing than many red wines, but shares some characteristics with white wine."},
+             { "node": 14, "message": "Zinfandel", "yes": 0, "no": 0 , "description": "Zinfandel is a very versatile grape. It has a zesty flavor with berry and pepper, and is a suitable full bodied wine for cold days. "},
+             { "node": 15, "message": "Moscato", "yes": 0, "no": 0 , "description": "Moscato pairs well with dessert and is delicious on it's own. It is sweet and fruity, with a characteristic grapefruity and musky aroma."},
+];
+
+// this is used for keep track of visted nodes when we test for loops in the tree
+var visited;
+
+// These are messages that Alexa says to the user during conversation
+
+// This is the intial welcome message
+var welcomeMessage = "Welcome to wine recommender, are you ready to begin?";
+
+// This is the message that is repeated if the response to the initial welcome message is not heard
+var repeatWelcomeMessage = "Say yes to start the recommendation or no to quit.";
+
+// this is the message that is repeated if Alexa does not hear/understand the reponse to the welcome message
+var promptToStartMessage = "Say yes to continue, or no to end the recommendation.";
+
+// This is the prompt during the recommender when Alexa doesnt hear or understand a yes / no reply
+var promptToSayYesNo = "Say yes or no to answer the question.";
+
+// This is the response to the user after the final question when Alex decides on what group choice the user should be given
+var decisionMessage = "I think you will enjoy a good";
+
+// This is the prompt to ask the user if they would like to hear a short description of their chosen wine or to begin again
+var playAgainMessage = "Say 'tell me more' to hear a short description of this wine. If you want another recommendation, say 'more wine please'!";
+
+// this is the help message during the setup at the beginning of the recommender
+var helpMessage = "I will ask you some questions that will identify which wine will be most suitable for you. Want to start now?";
+
+// This is the goodbye message when the user has asked to quit the recommender
+var goodbyeMessage = "Ok, see you next time! Cheers!";
+
+var speechNotFoundMessage = "Could not find the speech answer for this. Have another drink instead!";
+
+var nodeNotFoundMessage = "Could not find the right node for this message. How about a glass of red?";
+
+var descriptionNotFoundMessage = "Could not find description for this wine node. Have another bubbly on me!";
+
+var loopsDetectedMessage = "A repeated path was detected on the wine node tree, please fix before continuing. In the meantime, why don't you enjoy a glass of white wine!";
+
+var utteranceTellMeMore = "tell me more";
+
+// var utterancePlayAgain = "start again";
+var utterancePlayAgain = "more wine please";
+
+// the first node that we will use
+var START_NODE = 1;
+
+// --------------- Handlers -----------------------
+
+// Called when the session starts.
+exports.handler = function (event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(newSessionHandler, startGameHandlers, askQuestionHandlers, descriptionHandlers);
+    alexa.execute();
+};
+
+// set state to start up and  welcome the user
+var newSessionHandler = {
+    'NewSession': function () {
+        this.handler.state = states.STARTMODE;
+        this.emit(':ask', welcomeMessage, repeatWelcomeMessage);
+    }
+};
+
+// --------------- Functions that control the skill's behavior -----------------------
+
+// Called at the start of the recommender, picks and asks first question for the user
+var startGameHandlers = Alexa.CreateStateHandler(states.STARTMODE, {
+    'AMAZON.YesIntent': function () {
+
+        // ---------------------------------------------------------------
+        // check to see if there are any loops in the node tree - this section can be removed in production code
+        visited = [nodes.length];
+        var loopFound = helper.debugFunction_walkNode(START_NODE);
+        if( loopFound === true)
+        {
+             this.emit(':tell', loopsDetectedMessage);
+        }
+        // ---------------------------------------------------------------
+
+        // set state to asking questions
+        this.handler.state = states.ASKMODE;
+
+        // ask first question, the response will be handled in the askQuestionHandler
+        var message = helper.getSpeechForNode(START_NODE);
+
+        // record the node we are on
+        this.attributes.currentNode = START_NODE;
+
+        // ask the first question
+        this.emit(':ask', message, message);
+    },
+    'AMAZON.NoIntent': function () {
+        // Handle No intent.
+        this.emit(':tell', goodbyeMessage);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', goodbyeMessage);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', goodbyeMessage);
+    },
+    'AMAZON.StartOverIntent': function () {
+         this.emit(':ask', promptToStartMessage, promptToStartMessage);
+    },
+    'AMAZON.HelpIntent': function () {
+        this.emit(':ask', helpMessage, helpMessage);
+    },
+    'Unhandled': function () {
+        this.emit(':ask', promptToStartMessage, promptToStartMessage);
+    }
+});
+
+
+// user will have been asked a question when this intent is called. We want to look at their yes/no
+// response and then ask another question. If we have asked more than the requested number of questions Alexa will
+// make a choice, inform the user and then ask if they want to play again
+var askQuestionHandlers = Alexa.CreateStateHandler(states.ASKMODE, {
+
+    'AMAZON.YesIntent': function () {
+        // Handle Yes intent.
+        helper.yesOrNo(this,'yes');
+    },
+    'AMAZON.NoIntent': function () {
+        // Handle No intent.
+         helper.yesOrNo(this, 'no');
+    },
+    'AMAZON.HelpIntent': function () {
+        this.emit(':ask', promptToSayYesNo, promptToSayYesNo);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', goodbyeMessage);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', goodbyeMessage);
+    },
+    'AMAZON.StartOverIntent': function () {
+        // reset the recommender state to start mode
+        this.handler.state = states.STARTMODE;
+        this.emit(':ask', welcomeMessage, repeatWelcomeMessage);
+    },
+    'Unhandled': function () {
+        this.emit(':ask', promptToSayYesNo, promptToSayYesNo);
+    }
+});
+
+// user has heard the final choice and has been asked if they want to hear the description or to play again
+var descriptionHandlers = Alexa.CreateStateHandler(states.DESCRIPTIONMODE, {
+
+ 'AMAZON.YesIntent': function () {
+        // Handle Yes intent.
+        // reset the recommender state to start mode
+        this.handler.state = states.STARTMODE;
+        this.emit(':ask', welcomeMessage, repeatWelcomeMessage);
+    },
+    'AMAZON.NoIntent': function () {
+        // Handle No intent.
+        this.emit(':tell', goodbyeMessage);
+    },
+    'AMAZON.HelpIntent': function () {
+        this.emit(':ask', promptToSayYesNo, promptToSayYesNo);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', goodbyeMessage);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', goodbyeMessage);
+    },
+    'AMAZON.StartOverIntent': function () {
+        // reset the recommender state to start mode
+        this.handler.state = states.STARTMODE;
+        this.emit(':ask', welcomeMessage, repeatWelcomeMessage);
+    },
+    'DescriptionIntent': function () {
+        var reply = this.event.request.intent.slots.Description.value;
+        console.log('HEARD:' + reply);
+
+        if( reply === utteranceTellMeMore){
+            // If the user asks for "tell me more", then the description is given
+            helper.giveDescription(this);
+        } else if( reply === utterancePlayAgain) {
+            // reset the recommender state to start mode
+            this.handler.state = states.STARTMODE;
+            this.emit(':ask', welcomeMessage, repeatWelcomeMessage);
+        } else {
+             this.emit(':ask', playAgainMessage, playAgainMessage);
+        }
+    },
+    'Unhandled': function () {
+        this.emit(':ask', promptToSayYesNo, promptToSayYesNo);
+    }
+});
+
+// --------------- Helper Functions  -----------------------
+
+var helper = {
+
+    // gives the user more information on their final choice
+    giveDescription: function (context) {
+
+        // get the speech for the child node
+        var description = helper.getDescriptionForNode(context.attributes.currentNode);
+        var message = description + ' ' + repeatWelcomeMessage;
+
+        context.emit(':ask', message, message);
+    },
+
+    // logic to provide the responses to the yes or no responses to the main questions
+    yesOrNo: function (context, reply) {
+
+        // this is a question node so we need to see if the user picked yes or no
+        var nextNodeId = helper.getNextNode(context.attributes.currentNode, reply);
+
+        // error in node data
+        if (nextNodeId == -1)
+        {
+            context.handler.state = states.STARTMODE;
+
+            // the current node was not found in the nodes array
+            // this is due to the current node in the nodes array having a yes / no node id for a node that does not exist
+            context.emit(':tell', nodeNotFoundMessage, nodeNotFoundMessage);
+        }
+
+        // get the speech for the child node
+        var message = helper.getSpeechForNode(nextNodeId);
+
+        // have we made a decision
+        if (helper.isAnswerNode(nextNodeId) === true) {
+
+            // set the recommender state to description mode
+            context.handler.state = states.DESCRIPTIONMODE;
+
+            // append the play again prompt to the decision and speak it
+            message = decisionMessage + ' ' + message + ' , ' + playAgainMessage;
+        }
+
+        // set the current node to next node we want to go to
+        context.attributes.currentNode = nextNodeId;
+
+        context.emit(':ask', message, message);
+    },
+
+    // gets the description for the given node id
+    getDescriptionForNode: function (nodeId) {
+
+        for (var i = 0; i < nodes.length; i++) {
+            if (nodes[i].node == nodeId) {
+                console.log("here are the nodes - finding the description now")
+                console.log(nodes[i].description)
+                return nodes[i].description;
+            }
+        }
+        return descriptionNotFoundMessage + nodeId;
+    },
+
+    // returns the speech for the provided node id
+    getSpeechForNode: function (nodeId) {
+
+        for (var i = 0; i < nodes.length; i++) {
+            if (nodes[i].node == nodeId) {
+                return nodes[i].message;
+            }
+        }
+        return speechNotFoundMessage + nodeId;
+    },
+
+    // checks to see if this node is an choice node or a decision node
+    isAnswerNode: function (nodeId) {
+
+        for (var i = 0; i < nodes.length; i++) {
+            if (nodes[i].node == nodeId) {
+                if (nodes[i].yes === 0 && nodes[i].no === 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    },
+
+    // gets the next node to traverse to based on the yes no response
+    getNextNode: function (nodeId, yesNo) {
+        for (var i = 0; i < nodes.length; i++) {
+            if (nodes[i].node == nodeId) {
+                if (yesNo == "yes") {
+                    return nodes[i].yes;
+                }
+                return nodes[i].no;
+            }
+        }
+        // error condition, didnt find a matching node id. Cause will be a yes / no entry in the array but with no corrosponding array entry
+        return -1;
+    },
+
+    // Recursively walks the node tree looking for nodes already visited
+    // This method could be changed if you want to implement another type of checking mechanism
+    // This should be run on debug builds only not production
+    // returns false if node tree path does not contain any previously visited nodes, true if it finds one
+    debugFunction_walkNode: function (nodeId) {
+
+        // console.log("Walking node: " + nodeId);
+
+        if( helper.isAnswerNode(nodeId) === true) {
+            // found an answer node - this path to this node does not contain a previously visted node
+            // so we will return without recursing further
+
+            // console.log("Answer node found");
+             return false;
+        }
+
+        // mark this question node as visited
+        if( helper.debugFunction_AddToVisited(nodeId) === false)
+        {
+            // node was not added to the visited list as it already exists, this indicates a duplicate path in the tree
+            return true;
+        }
+
+        // console.log("Recursing yes path");
+        var yesNode = helper.getNextNode(nodeId, "yes");
+        var duplicatePathHit = helper.debugFunction_walkNode(yesNode);
+
+        if( duplicatePathHit === true){
+            return true;
+        }
+
+        // console.log("Recursing no");
+        var noNode = helper.getNextNode(nodeId, "no");
+        duplicatePathHit = helper.debugFunction_walkNode(noNode);
+
+        if( duplicatePathHit === true){
+            return true;
+        }
+
+        // the paths below this node returned no duplicates
+        return false;
+    },
+
+    // checks to see if this node has previously been visited
+    // if it has it will be set to 1 in the array and we return false (exists)
+    // if it hasnt we set it to 1 and return true (added)
+    debugFunction_AddToVisited: function (nodeId) {
+
+        if (visited[nodeId] === 1) {
+            // node previously added - duplicate exists
+            // console.log("Node was previously visited - duplicate detected");
+            return false;
+        }
+
+        // was not found so add it as a visited node
+        visited[nodeId] = 1;
+        return true;
+    }
+};
