f5267b5dd87105b8af2728fb372e87d8575d45ff
jadefeng <jade.jiefeng@gmail.com>
2016-10-07 00:49:17 -0700

    Added Cheese of the Day recommender

--- Aussie Slang/AussieSlangHowTo/index.js
+++ Aussie Slang/AussieSlangHowTo/index.js
@@ -3,7 +3,7 @@
 var Alexa = require('alexa-sdk');
 var APP_ID = 'arn:aws:lambda:us-east-1:444491341796:function:AussieSlangHowTo';
 var SKILL_NAME = 'Aussie Slang';
-var recipes = require('./recipes');
+var dictionary = require('./dictionary');
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
@@ -15,8 +15,7 @@ exports.handler = function(event, context, callback) {
 var handlers = {
     // 'NewSession': function () {
     'LaunchRequest': function () {
-        this.attributes['speechOutput'] = 'G\'day! Welcome to ' + SKILL_NAME + '. You can ask a question like, what\'s a' +
-            ' barbie? Now, what can I help you with?';
+        this.attributes['speechOutput'] = 'Welcome to ' + SKILL_NAME + '. You can ask a question like, what\'s 5a barbie? Now, what can I help you with?';
         // If the user either does not reply to the welcome message or says something that is not
         // understood, they will be prompted again with this text.
         this.attributes['repromptSpeech'] = 'For instructions on what you can ask, please say help me.';
@@ -30,17 +29,17 @@ var handlers = {
         }
 
         var cardTitle = SKILL_NAME + ' - Meaning of ' + wordName;
-        var recipe = recipes[wordName];
+        var recipe = dictionary[wordName];
 
         if (recipe) {
-            this.attributes['speechOutput'] = recipe;
+            this.attributes['speechOutput'] = recipe + "Are there any other words you want to know?";
             this.attributes['repromptSpeech'] = 'Try saying repeat.';
             this.emit(':askWithCard', recipe, this.attributes['repromptSpeech'], cardTitle, recipe);
         } else {
             var speechOutput = 'I\'m sorry mate, I currently do not know what that means.';
             var repromptSpeech = 'What else can I help with?';
             if (wordName) {
-                speechOutput = 'The meaning of ' + wordName + ' is ' + recipes[wordName] + ".";
+                speechOutput = 'The meaning of ' + wordName + ' is ' + dictionary[wordName] + ".";
             } else {
                 speechOutput = 'that meaning. ';
             }
